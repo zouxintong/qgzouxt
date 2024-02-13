dcdc79ac1a9fa20b2ebbc26e4241a3cf7c308de1
zouxintong <416351984@qq.com>
2024-02-13 15:10:11 +0800

    2024.2.13

---
+++ CODE/用户信息录入系统/用户信息录入系统/test.cpp
@@ -0,0 +1,60 @@
+#include "TeamMembers.h"
+
+int main() {
+    TeamMemberManager manager;
+    int choice;
+
+    while (true) {
+        // 显示功能选择菜单
+        cout << "用户信息管理系统" << endl;
+        cout << "1. 添加用户" << endl;
+        cout << "2. 删除用户" << endl;
+        cout << "3. 按姓名查找用户" << endl;
+        cout << "4. 修改用户信息" << endl;
+        cout << "5. 显示所有联系人信息" << endl;
+        cout << "6. 用户按编号排序" << endl;
+        cout << "7. 简单的模糊搜索" << endl;
+        cout << "8. 保存到文件" << endl;
+        cout << "9. 从文件加载" << endl;
+        cout << "0. 退出" << endl;
+        cout << "请输入您的选择：";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            manager.addMember();
+            break;
+        case 2:
+            manager.deleteMember();
+            break;
+        case 3:
+            manager.findMemberByName();
+            break;
+        case 4:
+            manager.modifyMember();
+            break;
+        case 5:
+            manager.displayAllMembers();
+            break;
+        case 6:
+            manager.sortMembersById();
+            break;
+        case 7:
+            manager.fuzzySearchByName();
+            break;
+        case 8:
+            manager.saveToFile();
+            break;
+        case 9:
+            manager.loadFromFile();
+            break;
+        case 0:
+            cout << "程序退出。" << endl;
+            return 0;
+        default:
+            cout << "无效选项，请重试。" << endl;
+        }
+    }
+
+    return 0;
+}
